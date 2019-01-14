b396ad0a14ebba25215d21dca6ce6e3772b9ea0f
tusharnimje21@gmail.com <tusharnimje21@gmail.com>
2019-01-14 22:43:20 +0530

    added menu and table section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'code-chalange2';
+
+  menuStatus = true;
+
+  constructor() { }
+
+  onMenuToggle(event: boolean) {
+    this.menuStatus = event;
+  }
 }
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  @Output() menuToggle = new EventEmitter<boolean>();
+
+  isMenuOpen = true;
+  menuList: Array<{ label: string, icon: string }>;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuList = [
+      { label: 'Home', icon: 'fa-home' },
+      { label: 'Library', icon: 'fa-list' },
+      { label: 'Self Service Utility', icon: 'fa-cog' }
+    ];
+  }
+
+  onMenuToggle() {
+    this.isMenuOpen = !this.isMenuOpen;
+    this.menuToggle.emit(this.isMenuOpen);
+  }
+
+}
